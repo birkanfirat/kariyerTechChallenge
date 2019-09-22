3f76829e434ff67b9f3f6e534236ba64908ad900
Birkan Fırat <bfirat@nuevo.com.tr>
2019-09-22 16:32:57 +0300

    adapter Create

--- app/src/main/java/com/example/kariyertechchallenge/Presenter/MarketListActivity.kt
+++ app/src/main/java/com/example/kariyertechchallenge/Presenter/MarketListActivity.kt
@@ -7,13 +7,19 @@ import com.example.kariyertechchallenge.Api.ApiClient
 import com.example.kariyertechchallenge.Api.ApiInterface
 import com.example.kariyertechchallenge.Api.Constants
 import com.example.kariyertechchallenge.Model.MarketingModel
+import com.example.kariyertechchallenge.Presenter.Adapter.MarketListAdapter
 import com.example.kariyertechchallenge.R
+import kotlinx.android.synthetic.main.activity_marketting.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.util.ArrayList
 
 class MarketListActivity : AppCompatActivity() {
     lateinit var apiInterface: ApiInterface
+    var markettingList: ArrayList<MarketingModel> = ArrayList<MarketingModel>()
+    lateinit var adapter: MarketListAdapter
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +27,10 @@ class MarketListActivity : AppCompatActivity() {
         setContentView(R.layout.activity_marketting)
 
         apiInterface = ApiClient.getClient().create(ApiInterface::class.java)
+
+        adapter = MarketListAdapter(this, markettingList)
+
+        markettingListView.setAdapter(adapter)
 
         init()
 
@@ -38,6 +48,10 @@ class MarketListActivity : AppCompatActivity() {
 
             override fun onResponse(call: Call<List<MarketingModel>>, response: Response<List<MarketingModel>>) {
                 Log.d("",response.body().toString())
+                if(!response.body().isNullOrEmpty()) {
+                    adapter.markettingList = response.body()!!
+                    adapter.notifyDataSetChanged()
+                }
 
 
             }
